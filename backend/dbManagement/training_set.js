51d2af0212d2ef0d8d5b99543af26378e3152d2e
Samuel Horner <samkoseyhorner@gmail.com>
2026-02-17 13:48:19 +0000

    Moved training set logic to a seperate file

--- backend/dbManagement/training_set.js
+++ backend/dbManagement/training_set.js
@@ -2,6 +2,47 @@ import csv from "csvtojson";
 
 import { manager, addHobby } from "./index.js"
 
+// Training set structure:
+// Questions: (1-5, normalised to -1, 1)
+// 1. Do you enjoy meeting new people?
+// 2. Do you enjoy working in a team?
+// 3. Do you enjoy exercising?
+// 4. Do you enjoy creating things?
+// 5. Do you enjoy challenging yourself?
+// 6. Do you enjoy spending your free time with other people?
+// 7. Do you enjoy learning new things?
+// 8. Do you enjoy seeing yourself improve?
+// 9. Do you enjoy being in nature?
+// 10. Do you enjoy performing?
+// 11. Do you enjoy competition?
+// 12. Do you enjoy collecting things?
+// 13. Do you enjoy speding time alone?
+// 14. Do you enjoy expressing your abilities and skills?
+// 15. Do you enjoy expressing your identity and interests?
+
+const training_set_table_schema = `
+CREATE TABLE IF NOT EXISTS TrainingSetTable (
+    RecordID INTEGER PRIMARY KEY,
+    HobbyID INTEGER,
+    Question1 FLOAT,
+    Question2 FLOAT,
+    Question3 FLOAT,
+    Question4 FLOAT,
+    Question5 FLOAT,
+    Question6 FLOAT,
+    Question7 FLOAT,
+    Question8 FLOAT,
+    Question9 FLOAT,
+    Question10 FLOAT,
+    Question11 FLOAT,
+    Question12 FLOAT,
+    Question13 FLOAT,
+    Question14 FLOAT,
+    Question15 FLOAT,
+    FOREIGN KEY (HobbyID) REFERENCES HobbyTable(HobbyID)
+);
+`;
+
 /**
  * Loads the training set from a CSV file.
  * @param {string} path 
@@ -17,16 +58,18 @@ async function loadTrainingSetFromCSV(path) {
     // [ "item1, Item2  ", 1, 2, 3, ...]
     // To:
     // [ ["item1", "item2"], 1, 2, 3, ...]
+    console.log(data);
+
     return data.map(row => row.splice(1))
                 .map(row => [row.shift().split(",")
-                .map(str => str.toLowerCase().trim())].concat(row));
+                .map(str => str.toLowerCase().trim().replace(".", ""))].concat(row));
 }
 
 /**
  * Initialises the training set, loading from a CSV file.
  */
-export async function loadTrainingSet() {
-    const training_data = await loadTrainingSetFromCSV("./backend/dbManagement/training_set.csv");
+export async function load(path) {
+    const training_data = await loadTrainingSetFromCSV(path);
 
     training_data.forEach(row => {
         const hobbies = row.shift();
@@ -56,7 +99,10 @@ export async function loadTrainingSet() {
     });
 }
 
-export async function getTrainingSet() {
+/**
+ * @returns All records from the training set  DB
+ */
+export async function get() {
     return await manager.dbGet(`
         SELECT 
             TrainingSetTable.HobbyID,
@@ -80,3 +126,25 @@ export async function getTrainingSet() {
         JOIN HobbyTable;
     `);
 }
+
+/**
+ * Initilaises the training set database, loading survey data from
+ * ./backend/data/traning_set.csv
+ * 
+ * Relies on the HobbyDB being initiliased.
+ */
+export async function init() {
+    await manager.dbExecute(training_set_table_schema);
+
+    // Load training set from CSV
+    const training_set_populated = (await manager.dbGet(`SELECT RecordID FROM TrainingSetTable;`)).length != 0;
+    if (!training_set_populated) {
+        await load("./backend/data/training_set.csv");
+    }
+}
+
+export default {
+    init,
+    load,
+    get
+};
