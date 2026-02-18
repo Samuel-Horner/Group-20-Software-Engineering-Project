62f23403b30cd2d78c02bdb461d734005ec9b6f8
Samuel Horner <samkoseyhorner@gmail.com>
2026-02-18 12:48:20 +0000

    Updated training set and improved hobby normalisation

--- backend/dbManagement/training_set.js
+++ backend/dbManagement/training_set.js
@@ -54,15 +54,17 @@ export async function loadTrainingSetFromCSV(path) {
         trim: true
     }).fromFile(path);
 
-
-
     // Maps array of the form:
     // [ "item1, Item2  ", 1, 2, 3, ...]
     // To:
     // [ ["item1", "item2"], -1, -0.5, 0, ...]
+    // console.log(data[0].splice(1).shift().split(/[,]|(and)/g));
+    
     return data.map(row => row.splice(1))
-        .map(row => [row.shift().split(",")
-            .map(str => str.toLowerCase().trim().replace(".", ""))]
+        .map(row => [row.shift().toLowerCase().split(/[,]|(\sand)/g)
+                .filter(str => str)
+                .map(str => str.replace(/\W/g, ''))
+            ]
             .concat(row.map(value => (value - 3) / 2)
             ));
 }
