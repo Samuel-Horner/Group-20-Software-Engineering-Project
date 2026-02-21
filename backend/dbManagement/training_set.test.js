c3bccee925199be5b752cc970ae84441962cc1df
Samuel Horner <samkoseyhorner@gmail.com>
2026-02-21 14:47:16 +0000

    Reworked training set tests

--- backend/dbManagement/training_set.test.js
+++ backend/dbManagement/training_set.test.js
@@ -3,7 +3,7 @@ import { loadTrainingSetFromCSV } from "./training_set.js";
 describe("Training Set Database", () => {
     test("Load CSV", async () => {
         expect(await loadTrainingSetFromCSV("./data/training_set.test.csv"))
-            .toStrictEqual([[["video games", "running", "gym", "playing guitar"], 1, 0, 1, 1, 0.5, 1, -1, -0.5, 1, 1, 0, 0.5, 0.5, 1, 0.5]]);
+            .toStrictEqual([[["video games", "running", "gym", "playing guitar"], 5, 3, 5, 5, 4, 5, 1, 2, 5, 5, 3, 4, 4, 5, 4]]);
 
         await expect(loadTrainingSetFromCSV("foo")).rejects.toThrow("File does not exist at foo. Check to make sure the file path to your csv is correct.");
     });
